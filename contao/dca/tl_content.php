0342b4366e2237e8946031bdf267d13af93f510f
Hamid Peywasti <hamid@respinar.com>
2023-11-17 15:21:00 +0330

    add ce to tl_content

---
+++ contao/dca/tl_content.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_DCA']['tl_content']['palettes']['swiper_slider'] = '
+    {type_legend},type,headline;
+    {template_legend:hide},customTpl;
+    {protected_legend:hide},protected;
+    {expert_legend:hide},guests,cssID;
+    {invisible_legend:hide},invisible,start,stop
+';
