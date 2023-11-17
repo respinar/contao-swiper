6aca886339459e70258ce5118a174b5b24472d2a
Hamid Peywasti <hamid@respinar.com>
2023-11-17 15:20:23 +0330

    create content element

---
+++ src/Controller/ContentElement/SwiperSliderController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Respinar\ContaoSwiperBundle\Controller\ContentElement;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AsContentElement(category: 'slider')]
+class SwiperSliderController extends AbstractContentElementController
+{
+    protected function getResponse(Template $template, ContentModel $model, Request $request): Response
+    {
+
+
+
+        $GLOBALS['TL_HEAD'][] = Template::generateScriptTag('bundles/respinarcontaoswiper/js/swiper-bundle.min.js', false, null);
+        $GLOBALS['TL_HEAD'][] = Template::generateStyleTag('bundles/respinarcontaoswiper/css/swiper-bundle.min.css', false, null);
+
+        return $template->getResponse();
+    }
+}
